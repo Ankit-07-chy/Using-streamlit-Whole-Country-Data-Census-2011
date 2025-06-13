3a70fce40d0c7196a6032d9561c9f406736c36c6
Ankit-07-chy <ankit07chy@gmail.com>
2025-06-13 18:15:48 +0530

    1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout='wide',
                    page_title="Country Census Analysis", 
